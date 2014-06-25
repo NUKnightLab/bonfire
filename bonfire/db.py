13aebfc8a4d07525bbb57530c2d4be60dd12ce18
Liam Andrew <landrew@mit.edu>
2014-06-24 23:36:08 -0400

    Adds check for existing user in ES before indexing.

--- bonfire/db.py
+++ bonfire/db.py
@@ -30,15 +30,18 @@ def es(universe):
 def build_universe_mappings(universe):
     es(universe).indices.put_mapping(UNPROCESSED_TWEET_DOCUMENT_TYPE,
         UNPROCESSED_TWEET_MAPPING)
-    
 
-def save_user(universe, user):
-    """Save a user to the universe index."""
+def index_user(universe, user):
+    """Add a user to the universe index."""
     es(universe).index(index=universe,
         doc_type=USER_DOCUMENT_TYPE,
         id=user['id'],
         body=user) 
 
+def update_user(universe, user):
+    """Update a user in the universe index."""
+    es(universe).update(index=universe, doc_type=USER_DOCUMENT_TYPE, id=user['id'],
+        body=user)
 
 def get_universe_users(universe, size=5000):
     """Get users for the universe."""
@@ -46,6 +49,25 @@ def get_universe_users(universe, size=5000):
         body={}, size=size)
     return res['hits']['hits']
 
+def get_user(universe, user):
+    """Get a user from the universe index."""
+    return es(universe).get(index=universe, doc_type=USER_DOCUMENT_TYPE, id=user['id'])
+
+def user_exists(universe, user):
+    """Check if a user exists in the universe index."""
+    return es(universe).exists(index=universe, doc_type=USER_DOCUMENT_TYPE, id=user['id'])
+
+def save_user(universe, user):
+    """Check if a user exists in the database. If not, create it. Otherwise, If so, update it if need be."""
+    if user_exists(universe, user):
+        old_user = get_user(universe, user)
+        if len(user.keys()) == 1 and len(old_user['_source'].keys()) > 1:
+            # We already have user metadata, don't update.
+            pass
+        else:
+            update_user(universe, user)
+    else:
+        index_user(universe, user)
 
 def enqueue_tweet(universe, tweet):
     """Save a tweet to the universe index as an uprocessed tweet document."""
